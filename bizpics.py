3e6da26b5d6781f0cdf479cb089d8ab88f1555f5
jpike97 <jpike@sagepath.com>
2020-08-24 17:10:00 -0400

    convert to hsv values

--- bizpics.py
+++ bizpics.py
@@ -34,13 +34,53 @@ def imagecalc():
 	index_max = scipy.argmax(counts)  # find most frequent
 	peak = codes[index_max]
 	colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
-	colourRGB = convertToRGB(colour)
-	colorlist.append(colourRGB)
+	colourHSV = convertToHSV(colour)
+	print("color HSV is ")
+	print(colourHSV)
+	colorlist.append(colourHSV)
 
-def convertToRGB(hexColor):
-        h = hexColor
-        rgb = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
-        return(rgb)
+def convertToHSV(rgbColor):
+		h = rgbColor
+		rgb = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+		r = rgb[0]
+		g = rgb[1]
+		b = rgb[2]
+
+		r, g, b = r / 255.0, g / 255.0, b / 255.0
+  
+	# h, s, v = hue, saturation, value 
+		cmax = max(r, g, b)    # maximum of r, g, b 
+		cmin = min(r, g, b)    # minimum of r, g, b 
+		diff = cmax-cmin       # diff of cmax and cmin. 
+  
+	# if cmax and cmax are equal then h = 0 
+		if cmax == cmin:  
+			h = 0
+	  
+	# if cmax equal r then compute h 
+		elif cmax == r:  
+			h = (60 * ((g - b) / diff) + 360) % 360
+  
+	# if cmax equal g then compute h 
+		elif cmax == g: 
+			h = (60 * ((b - r) / diff) + 120) % 360
+  
+	# if cmax equal b then compute h 
+		elif cmax == b: 
+			h = (60 * ((r - g) / diff) + 240) % 360
+  
+		# if cmax equal zero 
+		if cmax == 0: 
+			s = 0
+		else: 
+			s = (diff / cmax) * 100
+  
+	# compute v 
+		v = cmax * 100
+		
+		hsvTuple = (h,s,v)
+		print(hsvTuple)
+		return hsvTuple
 
 
 
@@ -54,7 +94,7 @@ driver.get("http://4chan.org/biz/catalog")
 images = driver.find_elements_by_tag_name('img')
 
 for image in images:
-	urllib.request.urlretrieve(image.get_attribute('src'), 'img/image' + '.jpg')
+	urllib.request.urlretrieve(image.get_attribute('src'), 'img/image.jpg')
 	try:
 		imagecalc()
 	except:
@@ -68,18 +108,37 @@ print(colorlist)
 redTotal = 0
 greenTotal = 0
 imageTotal = 0
+hueTotal = 0
+satTotal = 0
+vTotal = 0
 
 for rgbValue in colorlist:
-	green = rgbValue[0]
-	red = rgbValue[1]
-	greenTotal += green
-	redTotal += red
+	hue = rgbValue[0]
+	sat = rgbValue[1]
+	v = rgbValue[2]
+
+	##grey / blackness check
+	if sat and v > 30:
+		##check for red / green
+		if hue > 0 and hue < 60:
+			redTotal += 1
+			
+		elif hue > 100 and hue < 180:
+			greenTotal += 1
+
+	hueTotal += hue
+	satTotal += sat
+	vTotal += v
 	imageTotal += 1
 	
-greenAvg = greenTotal / imageTotal
-redAvg = redTotal / imageTotal
-##TODO check for what makes an image green lol 
+hueAvg = hueTotal / imageTotal
+satAvg = satTotal / imageTotal
+vAvg = vTotal / imageTotal
+##TODO: show reddest & greenest image lol?
 
-print(greenAvg)
-print(redAvg)
+print(greenTotal)
+print(redTotal)
 print(imageTotal)
+print(hueAvg)
+print(satAvg)
+print(vAvg)
